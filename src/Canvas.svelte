6469d6b770c647eb3f656ca2bcdde22c3b6c6322
dotsenko <dotsenko.vitaliy@gmail.com>
2019-05-24 11:11:20 +0300

    draw grid

--- src/Canvas.svelte
+++ src/Canvas.svelte
@@ -1,3 +1,35 @@
+<script>
+    import { onMount } from 'svelte';
+    let canvas;
+    let ctx;
+
+    const fieldSize = 800;
+    const numberOfCellsInRow = 50;
+    const framesPerSecond = 8;
+    $: cellSize = fieldSize / numberOfCellsInRow;
+
+    const drawGridLine = (ctx, xb, yb, xe, ye) => {
+        ctx.lineWidth = 1;
+        ctx.beginPath();
+        ctx.moveTo(xb,yb);
+        ctx.lineTo(xe,ye);
+        ctx.stroke();
+    }
+
+    const drawGrid = (ctx) => {
+        for(let i = 0; i <= fieldSize; i += cellSize) {
+            drawGridLine(ctx, i, 0, i, fieldSize);
+            drawGridLine(ctx, 0, i, fieldSize, i);
+        }
+    }
+
+    onMount(() => {
+        canvas = document.getElementById('canvas');
+        ctx = canvas.getContext('2d');
+        drawGrid(ctx);
+    });
+</script>
+
 <style>
     #canvas {
         display: block;
