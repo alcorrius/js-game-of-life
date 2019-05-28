d76d12d87dc7e8a2bae636aa425119cd0d0ed960
dotsenko <dotsenko.vitaliy@gmail.com>
2019-05-28 11:21:32 +0300

    Fill grid with random values

--- src/Canvas.svelte
+++ src/Canvas.svelte
@@ -3,6 +3,7 @@
 
     let canvas;
     let ctx;
+    let grid = [];
 
     const fieldSize = 800;
     const numberOfCellsInRow = 50;
@@ -23,10 +24,30 @@
         }
     }
 
+    let randomFill = (ctx) => {
+        for (let i=0; i < numberOfCellsInRow; i++) {
+            grid.push([[]]);
+            for (let j = 0; j < numberOfCellsInRow; j++) {
+                grid[i][j] = Math.random() > 0.9 ? 1 : 0;
+                if (grid[i][j] == 1) {
+                    ctx.beginPath();
+                    ctx.arc(cellSize/2 + i * cellSize, cellSize/2 + j * cellSize, 5, 0, 2 * Math.PI);
+                    ctx.fill();
+                }
+            }
+        }
+    }
+
+    let start = (ctx) => {
+        console.log("started");
+    }
+
     onMount(() => {
         canvas = document.getElementById('canvas');
         ctx = canvas.getContext('2d');
         drawGrid(ctx);
+        randomFill(ctx);
+        start(ctx);
     });
 </script>
 
